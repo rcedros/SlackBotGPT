cef1f01de29bd4f0cd434671b126f112597f6f75
Rubens Cedro <rcedros@gmail.com>
2023-08-12 01:50:30 -0300

    Update chatbot_slack_chatgpt.py

--- chatbot_slack_chatgpt.py
+++ chatbot_slack_chatgpt.py
@@ -7,19 +7,19 @@ from refatorador import process_code, process_message, info
 app = App(token=SLACK_BOT_TOKEN, name="neon-gpt")
 logger = logging.getLogger(__name__)
 
-@app.message(r"@SlackGPT")
+@app.message(r"@Hibot")
 def start_conversation(message, say):
     user_id = message['user']
     info[user_id] = {'type': 'random', 'thread': message['ts']}
     say("Olá, Como posso ajudar?", thread_ts=message['ts'])
 
-@app.message(r"@SlackGPT-security")
+@app.message(r"@code-security")
 def ask_security_questions(message, say):
     user_id = message['user']
     info[user_id] = {'type': 'security', 'thread': message['ts']}
     say("Qual é o nome da vulnerabilidade encontrada?", thread_ts=message['ts'])
 
-@app.message(r"@SlackGPT-refactor")
+@app.message(r"@code-refactor")
 def ask_security_questions(message, say):
     user_id = message['user']
     info[user_id] = {'type': 'refactor', 'thread': message['ts']}
